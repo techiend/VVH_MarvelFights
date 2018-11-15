3c8d8d688a3de20e1f3b7c6927471bd991c95b0a
Carlos Verde <cverde@ubiipagos.com>
2018-11-14 20:34:36 -0400

    Initial commit

---
+++ src/DBHelper/DBClass.java
@@ -0,0 +1,41 @@
+/*
+ * Contenido creado por los integrantes
+ * del Grupo #2, prohibida su reutilizaci�n.
+ * 
+ *  - Alessandra Varisco
+ *  - Carlos Hurtado
+ *  - Carlos Verde
+ */
+package DBHelper;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+/**
+ *
+ * @author Grupo2
+ */
+public class DBClass {
+    
+    private static HikariDataSource db;
+    static {
+        
+        Properties props = new Properties();
+        props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+        props.setProperty("dataSource.user", "cverde");
+        props.setProperty("dataSource.password", "123456");
+        props.setProperty("dataSource.databaseName", "testCarlos");
+        props.setProperty("maximumPoolSize", "2");
+        props.put("dataSource.logWriter", new PrintWriter(System.out));
+        
+        HikariConfig config = new HikariConfig(props);
+
+        db = new HikariDataSource(config);
+    }
+    public static Connection getConn() throws SQLException {
+        return db.getConnection();
+    }
+}
---
+++ src/Pruebas/PruebaSQL.java
@@ -0,0 +1,58 @@
+/*
+ * Contenido creado por los integrantes
+ * del Grupo #2, prohibida su reutilizaci�n.
+ * 
+ *  - Alessandra Varisco
+ *  - Carlos Hurtado
+ *  - Carlos Verde
+ */
+package Pruebas;
+
+import DBHelper.DBClass;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PruebaSQL {
+    
+        /*
+        *
+        *   Prueba de como hacer la conexion con la DB y como realizar una consulta simple
+        *
+        */
+    
+    public static void main(String[] args) {   
+        
+        try(
+            Connection conn = DBClass.getConn();
+                
+            // En caso de necesitar llenar algun campo con informacion especifica colocamos '?'
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM usuario WHERE apellido = ? or nombre = ?;");
+        ){
+            
+            // Al colocar '?' leugo simplemente especificamos que va en cada uno de ellos
+            pst.setString(1, "Varisco");
+            pst.setString(2, "Carlos");
+            // Pedimos ejecutar el query..
+            ResultSet rst = pst.executeQuery();
+            
+            // Ciclo mientras exista algo en la respuesta de la consulta
+            while (rst.next()){
+                
+                // Por cada una de las filas que retorna el SELECT utilizamos la informacion como nos plazca
+                System.out.println("\nUsuario #:"+rst.getInt("id")+"");
+                System.out.println("Nombre y Apellido: "+rst.getString("nombre")+" "+rst.getString("apellido"));
+                System.out.println("Telefono: "+rst.getString("telefono"));
+                System.out.println("Profesion: "+rst.getString("profesion"));
+            }
+            
+            // Cerramos la conexion para no desperdiciar recursos
+            conn.close();
+        }
+        catch (SQLException ex) {
+             ex.printStackTrace();
+        }
+    }
+        
+}
